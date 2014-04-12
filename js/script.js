99ab6d61302db1246dec017fb2da1709bb85de26
puhrome <phirom.yim@fullsail.edu>
2014-04-11 21:12:25 -0400

    Expression_worksheet_Dog_years
    Using WebStorm to write Expression of dog years in HTML and JS.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//Dog Years by Phirom Yim 4/11/14
+var sparkyAge; //declare Sparky's age
+sparkyAge = 4; //define Sparky's age as 4 years old in human years
+var dogYears; //declare dog years
+dogYears = 7; //define dog years as 7
+x = sparkyAge * dogYears; // Sparky's age in human years times 7 dog years
+x = 7 * 4;
+console.log(x);
+alert(x);
+
